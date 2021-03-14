392177023f0ae8ca0543c51d572aea001c3b95d4
sjkim <rlatjrtnwls@naver.com>
2021-03-14 11:43:38 +0900

    Final update!

--- app.js
+++ app.js
@@ -4,18 +4,31 @@
 // 상수canvas 객체에 jsCanvas 문서 삽입
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("jsColor")
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 // canvas size
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c"
+// canvas default bacaground color
+ctx.fillStyle="white"
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+ctx.strokeStyle = "INITIAL_COLOR"
 ctx.lineWidth = 2.5;
+
+// 색칠
+ctx.fillStyle = "INITIAL_COLOR";
 
 // default=false 하지만 마우스를 클릭(onMouseDown)하는 동안은 true
 let painting = false;
-
+let filling = false;
 
 
 function startPainting() {
@@ -60,14 +73,61 @@ function onMouseUp(event) {
 }
 
 
-function handleColorClick(event){
-    const color=event.target.style.backgroundColor;
-    // ctx.color를 바꿔야 선 색이 바뀌므로 ctx.color을 override
-    ctx.strokeStyle=color;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    // Override ctx.color
+    ctx.strokeStyle = color;
 
     //확인용
     //console.log(color);
+
+    // 색칠
+    ctx.fillStyle = color;
 }
+
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    // Override lineWidth
+    ctx.lineWidth = size;
+}
+
+
+function handleModeClick() {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+        ctx.fillStyle = ctx.strokeStyle;
+    }
+}
+
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+// 우클릭 방지
+function handleCM(event){
+    event.preventDefault();
+}
+
+// 저장하기
+function handleSaveClick(event){
+    //console.log(image);
+    const image=canvas.toDataURL("image/png");
+    const link=document.createElement("a");
+    link.href=image;
+    link.download="PaintJS";
+    //console.log(link);
+    link.click();
+}
+
+
+
 
 
 
@@ -77,10 +137,25 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
-Array.from(colors).forEach(color=>
+Array.from(colors).forEach(color =>
     //여기서의 color은 div의 개념. 그냥 묶는 개념이므로 어떤 단어를 쓰든 의미 및 영향 없음.
-    color.addEventListener("click",handleColorClick
+    color.addEventListener("click", handleColorClick
     )
 );
+
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
